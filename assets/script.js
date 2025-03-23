12a882d29fbfca25621763862e0b7884a8482912
verychat88 <whitsssssss@naver.com>
2025-03-23 22:36:58 +0900

    Delete assets directory

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleSubmenu(id) {
-  const el = document.getElementById(id);
-  if (el.style.display === "block") {
-    el.style.display = "none";
-  } else {
-    el.style.display = "block";
-  }
-}
